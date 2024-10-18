d5822d0ec72d399df7cdbae0e9df4593d5ae7be4
Mario <mariotek2002@gmail.com>
2024-10-18 21:56:53 +0800

    edit navbar

--- app/src/components/Navbar.vue
+++ app/src/components/Navbar.vue
@@ -1,10 +1,28 @@
 
 
 <template>
-    <nav>
-        <RouterLink :to="{ name: 'Home'}">Logo</RouterLink> <!-- this is logo -->
-        <RouterLink :to="{ name: 'Home'}">Home</RouterLink>
-        <RouterLink :to="{ name: 'Procedure'}">Procedure</RouterLink>
-        <RouterLink :to="{ name: 'Form'}">Admission</RouterLink>
-    </nav>
+    <div>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light p-3">
+            <RouterLink :to="{ name: 'Home'}" class="nav-link">
+                <img src="../assets/logo.png" alt="logo" width="75">
+            </RouterLink>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+          
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                  <RouterLink :to="{ name: 'Home'}" class="nav-link">Home</RouterLink>
+                </li>
+                <li class="nav-item active">
+                  <RouterLink :to="{ name: 'Procedure'}" class="nav-link">Procedure</RouterLink>
+                </li>
+                <li class="nav-item active">
+                  <RouterLink :to="{ name: 'Form'}" class="nav-link">Form</RouterLink>
+                </li>
+              </ul>
+            </div>
+          </nav>
+    </div>
 </template>
