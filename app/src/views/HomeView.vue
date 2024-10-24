0fa617f8e1681ffd9db632a44f479f9b629c93f2
Mario <mariotek2002@gmail.com>
2024-10-25 04:43:24 +0800

    adjust font color for visuals

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -5,12 +5,12 @@
       <div class="wrapper">
         <div class="title">
             <h1><span class="highlight px-3">2024-2025</span> <br> ESSU Admission <br> System</h1>
-            <h5>Secure your application here at <span class="yellow">Eastern Samar State University</span>. We offer innumerable opportunities for aspiring students.</h5>
+            <h5>Secure your application here at <span class="highlight px-1">Eastern Samar State University</span>. We offer innumerable opportunities for aspiring students.</h5>
             <div class="buttons">
               <button type="button" class="btn btn-outline-success">Enroll</button>
-              <button type="button" class="btn btn-secondary">Learn more</button>
+              <button type="button" class="btn btn-primary">Learn more</button>
             </div>
-            <p>*Our admission process is designed to be <strong>simple</strong> and <strong>efficient</strong>, ensuring that your journey with us begins smoothly.</p>
+            <small>*Our admission process is designed to be <strong>simple</strong> and <strong>efficient</strong>, ensuring that your journey with us begins smoothly.</small>
         </div>
       </div>
     </div>
@@ -22,6 +22,7 @@
     background-image: url('../assets/ESSU-campus.jpg');
     background-repeat: no-repeat;
     background-size: cover;
+    color: #F0f0E1;
   }
   .wrapper {
     display: grid;
@@ -38,17 +39,19 @@
     text-transform: uppercase;
     font-weight: 900;
     font-size: 4.5rem;
-    color: #014422;
   }
   .title h5 {
-    font-weight: 700;
-  }
-  .title p {
+    font-size: 1rem;
     font-weight: 500;
-    font-size: 0.6rem;
+  }
+  .title small {
+    font-weight: 300;
+    font-size: 0.7rem;
+    position: relative;
+    top: 1rem;
   }
   .highlight {
-    background: #ffee8c;
+    background: #014422;
   }
   .yellow {
     color: #014422;
@@ -57,14 +60,14 @@
     padding: 1rem 0 0.3rem 0;
   }
   .buttons button {
-    padding: 0.6rem 2rem;
+    padding: 0.7rem 3rem;
   }
-  .buttons .btn-secondary {
+  .buttons .btn-primary {
     margin-left: 1rem;
   }
   .overlay {
     width: 100%;
     height: 100%;
-    background: rgba(0, 0, 0, 0.4);
+    background: rgba(0, 0, 0, 0.8);
   }
 </style>
