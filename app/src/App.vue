5ac20bd7a6c1d8c389dfc16c5dedbd54b88bc002
Mario <mariotek2002@gmail.com>
2024-10-25 05:31:37 +0800

    added icons in footer

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,15 +1,17 @@
 <script setup>
 import { RouterView } from 'vue-router'
 import Navbar from './components/Navbar.vue'
+import Footer from './components/Footer.vue'
 </script>
 
 <template>
     <div class="wrapper">
       <Navbar />
       <RouterView />
+      <Footer />
     </div>
 </template>
 
 <style scoped>
-
+  
 </style>
