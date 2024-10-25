65b9f7b3230c46889863a76a995355b7e2ac9d18
Mario <mariotek2002@gmail.com>
2024-10-25 20:16:24 +0800

    added feature section

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -5,12 +5,16 @@
       <div class="wrapper">
         <div class="title">
             <h1><span class="highlight px-3">2024-2025</span> <br> ESSU Admission <br> System</h1>
-            <h5>Secure your application here at <span class="highlight px-1">Eastern Samar State University</span>. We offer innumerable opportunities for aspiring students.</h5>
+            <h5>In <span class="highlight px-1">Eastern Samar State University</span>, we offer innumerable opportunities for aspiring students.</h5>
             <div class="buttons">
               <button type="button" class="btn btn-outline-success">Enroll</button>
               <button type="button" class="btn btn-primary">Learn more</button>
             </div>
             <small>*Our admission process is designed to be <strong>simple</strong> and <strong>efficient</strong>, ensuring that your journey with us begins smoothly.</small>
+        </div>
+        <div class="features">
+          <h2>Features</h2>
+          <hr>
         </div>
       </div>
     </div>
@@ -23,6 +27,7 @@
     background-repeat: no-repeat;
     background-size: cover;
     color: #F0f0E1;
+    height: 100vh;
   }
   .wrapper {
     width: 100%;
@@ -30,12 +35,11 @@
     display: grid;
     grid-template-columns: repeat(4, 1fr);
     grid-auto-rows: minmax(3rem, auto);
-    padding: 4.5rem 6rem;
   }
   .title {
     padding: 1rem;
     grid-column: 1 / 4;
-
+    padding: 6rem 1.5rem 6rem 4.5rem;
   }
   .title h1 {
     text-transform: uppercase;
@@ -71,5 +75,18 @@
     width: 100%;
     height: 100%;
     background: rgba(0, 0, 0, 0.8);
+  }
+  .features {
+    grid-column: 4 / 5;
+    grid-row: 1;
+    border: 1px solid rgba(255, 255, 255, 0.4);
+    margin: 6rem 3rem 6rem 2rem;
+    width: 25rem;
+    border-radius: 1rem;
+    padding: 2rem;
+  }
+  .features h2 {
+    font-weight: 400;
+    font-size: 1.5rem;
   }
 </style>
