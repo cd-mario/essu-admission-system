7f6dca88d1af388f03758d58985d6eabeb9d1aca
Mario <mariotek2002@gmail.com>
2024-10-26 15:21:06 +0800

    added table for procedure

--- app/src/views/ProcedureView.vue
+++ app/src/views/ProcedureView.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="container p-5">
     <h1 class="title">Admission Procedure</h1>
+
+    <div class="card">
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">Cras justo odio</li>
+        <li class="list-group-item">Dapibus ac facilisis in</li>
+        <li class="list-group-item">Vestibulum at eros</li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -16,6 +24,10 @@
     width: fit-content;
     margin: 0 auto;
     padding-bottom: 1rem;
+  }
+  .card {
+    width: 40rem;
+    margin: 3rem auto;
   }
 
 </style>
