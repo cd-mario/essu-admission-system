3b475fd31a616f7609498e71528f995ba5678df1
Mario <mariotek2002@gmail.com>
2024-10-26 15:15:14 +0800

    added prodcedure title

--- app/src/views/ProcedureView.vue
+++ app/src/views/ProcedureView.vue
@@ -1,8 +1,21 @@
 <template>
-  <div class="container">
-    <h1>Admission Procedure</h1>
+  <div class="container p-5">
+    <h1 class="title">Admission Procedure</h1>
   </div>
 </template>
 
-<style>
+<style scoped>
+
+  .container {
+    text-align: center;
+  }
+  .title {
+    color: black;  
+    font-weight: 700;
+    border-bottom: 4px solid #014422;
+    width: fit-content;
+    margin: 0 auto;
+    padding-bottom: 1rem;
+  }
+
 </style>
