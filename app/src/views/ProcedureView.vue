a5a5d6ed75a3e6d4b693b0b664d1d345ec70bd95
Mario <mariotek2002@gmail.com>
2024-10-26 15:53:33 +0800

    fixed spacings of the number and the lists

--- app/src/views/ProcedureView.vue
+++ app/src/views/ProcedureView.vue
@@ -2,12 +2,18 @@
   <div class="container p-5">
     <h1 class="title">Admission Procedure</h1>
 
-    <div class="card">
-      <ul class="list-group list-group-flush">
-        <li class="list-group-item">Cras justo odio</li>
-        <li class="list-group-item">Dapibus ac facilisis in</li>
-        <li class="list-group-item">Vestibulum at eros</li>
-      </ul>
+    
+    <div class="standards">
+      <h3>Entrance standards</h3>
+      <div class="card">
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item"> 
+            <span class="num-list">1.</span> The University accepts applications from all high school graduates who meet the necessary qualifications and GPA requirements.
+          </li>
+          <li class="list-group-item">Dapibus ac facilisis in</li>
+          <li class="list-group-item">Vestibulum at eros</li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
@@ -25,9 +31,32 @@
     margin: 0 auto;
     padding-bottom: 1rem;
   }
-  .card {
-    width: 40rem;
-    margin: 3rem auto;
+  .standards {
+    margin: 3.4rem auto;
+    text-align: left;
+    width: 44rem;
   }
-
+  .standards h3 {
+    font-size: 1.3rem;
+    color: #014422;
+  }
+  .card {
+    width: 100%;
+    margin: 0 auto;
+  }
+  .card .list {
+    position: relative;
+    left: 2rem;
+    padding: 1rem;
+    border-bottom: 1px solid rgba;
+  }
+  .num-list {
+    position: absolute;
+    left: 1.2rem;
+    top: 3rem;
+  }
+  .list-group-item {
+    padding: 2rem 2rem 2rem 4rem;
+    line-height: 2rem;
+  }
 </style>
