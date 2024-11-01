55012898c0348fd348b89396ecfe7d2ba3ce7c38
Mario <mariotek2002@gmail.com>
2024-11-02 05:06:42 +0800

    added a note to form page

--- app/src/views/FormView.vue
+++ app/src/views/FormView.vue
@@ -2,6 +2,7 @@
     <div class="wrapper">
         <form>
             <h1 class="title">ESSU Admission Form</h1>
+            <p class="note"><strong>Note</strong>: You must login to access admission form.</p>
             <div class="input-wrapper">
                 <div class="form-group">
                   <label for="f_name">First Name: </label>
@@ -39,5 +40,14 @@
     }
     .input-wrapper {
         padding: 1rem 4rem;
+    }
+    .note {
+        position: relative;
+        left: 4rem;
+        border-radius: 0.2rem;
+        padding: 1rem 0.6rem;
+        border-left: 5px solid #4b6043;
+        background: rgba(0, 0, 0, 0.1);
+        width: 30rem;
     }
 </style>
