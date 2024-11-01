bda0a794f7a6eeed46f4480520c1acd77fa9a9c5
Mario <mariotek2002@gmail.com>
2024-11-02 04:53:57 +0800

    changed the spacing

--- app/src/views/FormView.vue
+++ app/src/views/FormView.vue
@@ -2,15 +2,17 @@
     <div class="wrapper">
         <form>
             <h1 class="title">ESSU Admission Form</h1>
-            <div class="form-group">
-              <label for="f_name">First Name: </label>
-              <input type="text" class="form-control" id="f_name" placeholder="First Name">
+            <div class="input-wrapper">
+                <div class="form-group">
+                  <label for="f_name">First Name: </label>
+                  <input type="text" class="form-control" id="f_name" placeholder="First Name">
+                </div>
+                <div class="form-check">
+                  <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                  <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
             </div>
-            <div class="form-check">
-              <input type="checkbox" class="form-check-input" id="exampleCheck1">
-              <label class="form-check-label" for="exampleCheck1">Check me out</label>
-            </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
           </form>
     </div>
 </template>
@@ -24,7 +26,6 @@
         width: 70%;
         margin: 0 auto;
         border: 1px solid rgba(0, 0, 0, 0.2);
-
     }
     .title {
         text-align: center;
@@ -33,9 +34,10 @@
         font-weight: 700;
         margin-bottom: 2rem;
         background: #4b6043;
-        background: -webkit-linear-gradient(101deg, #4b6043 0%, #9a8933 100%);
-        background: linear-gradient(101deg, #4b6043 0%, #9a8933 100%);
         padding: 1rem;
         color: #eee;
+    }
+    .input-wrapper {
+        padding: 1rem 4rem;
     }
 </style>
