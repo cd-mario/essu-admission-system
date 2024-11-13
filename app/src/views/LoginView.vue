4ea5abb610f3cea1b1a0f371bcd06e1f51e37402
Mario <mariotek2002@gmail.com>
2024-11-14 06:03:50 +0800

    added bg login page

--- app/src/views/LoginView.vue
+++ app/src/views/LoginView.vue
@@ -1,8 +1,8 @@
 <template>
+    <!-- need to remove navbar and footer in login page -->
     <div class="container">
         <div id="grid">
             <div class="image">
-                this is image
             </div>
             <div class="login">
                 this is login
@@ -16,7 +16,6 @@
     height: 100vh;
 }
 #grid {
-    border: 1px solid black;
     display: grid;
     grid-template-columns: repeat(10, 1fr);
     grid-auto-rows: minmax(100px, auto);
@@ -24,11 +23,14 @@
     height: 85%;
     border-radius: 2rem;
 }
-.image, .login {
-    border: 1px solid black;
-}
 .image {
     grid-column: 1 / 6;
+    background-image: url('../assets/images/login-bg.jpg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 100%;
+    border-top-left-radius: 2rem;
+    border-bottom-left-radius: 2rem;
 }
 .login {
     grid-column: 6 / 11;
