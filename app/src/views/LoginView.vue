8685d70b0341ead057edf098c31320a7f3be9530
Mario <mariotek2002@gmail.com>
2024-11-14 07:07:29 +0800

    Login page interface done

--- app/src/views/LoginView.vue
+++ app/src/views/LoginView.vue
@@ -5,7 +5,20 @@
                 <div class="image">
                 </div>
             <div class="login">
-                this is login
+                <h1 class="login-title">
+                    ESSU Admission System
+                </h1>
+                <form action="">
+                    <h2>Login</h2>
+                    <p>Input your details to sign in to you account.</p>
+
+                    <input type="text" placeholder="Enter your name/email" required>
+                    <input type="password" placeholder="Enter your password" required>
+                    <br>
+                    <button type="button" class="btn btn-primary mt-3">Login</button>
+                    <br>
+                    <small>Don't have an account? <a href="#">Register here</a></small>
+                </form>
             </div>
         </div>
     </div>
@@ -35,6 +48,52 @@
 
 }
 .login {
-    grid-column: 6 / 11;
+    grid-column: 5 / 11;
+    padding: 3rem 1rem;
+}
+.login-title {
+    color: white;
+    background: #f8d731;
+    font-weight: 300;
+    font-size: 1.4rem;
+    text-align: center;
+    text-transform: uppercase;
+    margin: 0 auto;
+    border-radius: 0.8rem;
+    width: fit-content;
+    padding: 0.5rem 2rem;
+    box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+} form {
+    width: 80%;
+    margin: 3rem auto;
+    text-align: center;
+}
+form h2 {
+    color: #f8d731;
+    font-size: 1.3rem;
+
+}
+form p {
+    font-size: smaller;
+    color: grey;
+    margin: 1rem 0 2rem 0;
+}
+input {
+    height: 2.5rem;
+    width: 65%;
+    padding: 0.5rem;
+    border-radius: 0.4rem;
+    border: 1px solid rgba(0, 0, 0, 0.3);
+    font-size: 0.9rem;
+    margin-bottom: 1rem;
+}
+input:placeholder-shown {
+    font-size: small;
+}
+button {
+    width: 65%;
+}
+small {
+    font-size: 0.8rem;
 }
 </style>
