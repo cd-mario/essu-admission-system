e7c018c2e7aa93d36cc2ba5507fd8e175a701b35
Mario <mariotek2002@gmail.com>
2024-11-14 05:37:19 +0800

    added the initial layout of the login page

--- app/src/views/LoginView.vue
+++ app/src/views/LoginView.vue
@@ -1,3 +1,25 @@
 <template>
-    <h1>This is login page!</h1>
+    <div class="container">
+        <div id="grid">
+            <div class="image">
+                this is image
+            </div>
+            <div class="login">
+                this is login
+            </div>
+        </div>
+    </div>
 </template>
+<style scoped>
+.container {
+    width: 100%;
+    height: 100vh;
+}
+#grid {
+    border: 1px solid black;
+    display: grid;
+    grid-template-columns: repeat(10, 1fr);
+    grid-auto-rows: minmax(100px, auto);
+    margin: 5rem 2rem;
+}
+</style>
