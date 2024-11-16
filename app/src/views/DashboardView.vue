e214e94d4d4c8f015a73600c1d75eff0f83e2a2e
Mario <mariotek2002@gmail.com>
2024-11-16 19:36:19 +0800

    added sidebar to dashboard page

--- app/src/views/DashboardView.vue
+++ app/src/views/DashboardView.vue
@@ -1,6 +1,28 @@
 <template>
-    
+    <div class="wrapper">
+        <div id="grid">
+            <div class="sidebar border">
+
+            </div>
+        </div>
+    </div>
 </template>
 <style scoped>
-
+.wrapper {
+    width: 100%;
+    height: 100vh;
+}
+#grid {
+    display: grid;
+    grid-template-columns: repeat(12, 1fr);
+    grid-auto-rows: minmax(15rem, auto);
+}
+.border {
+    border: 1px solid black;
+}
+.sidebar {
+    grid-column: 1 / 3;
+    grid-row: 1 / 4;
+    width: 17rem;
+}
 </style>
