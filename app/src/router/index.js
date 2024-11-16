7c37dbef975f16a6259b310ac16e29688952e002
Mario <mariotek2002@gmail.com>
2024-11-16 19:27:15 +0800

    added dashboard page and intialized its route

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -4,6 +4,7 @@ import FormView from '../views/FormView.vue'
 import LoginView from '../views/LoginView.vue'
 import RegisterView from '../views/RegisterView.vue'
 import StudentPortalView from '../views/StudentPortalView.vue'
+import DashboardView from '../views/DashboardView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -32,6 +33,11 @@ const router = createRouter({
       path: '/portal',
       name: 'Portal',
       component: StudentPortalView
+    },
+    {
+      path: '/dashboard',
+      name: 'Dashboard',
+      component: DashboardView
     },
     {
       path: '/procedure',
