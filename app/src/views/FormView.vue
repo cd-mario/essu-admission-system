7d87c86e953fa6a35cc9ad759676eb599db420df
Mario <mariotek2002@gmail.com>
2024-11-16 19:16:26 +0800

    added wrapper for the admission form

---
+++ app/src/views/FormView.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="wrapper">
+        <h1 class="title">ESSU Admission Form</h1>
+        <form action="">
+            
+        </form>
+    </div>
+</template>
+<style scoped>
+.wrapper {
+    width: 65%;
+    border-radius: 0.7rem;
+    margin: 2rem auto;
+    box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+}
+form {
+    padding: 2rem;
+}
+</style>
