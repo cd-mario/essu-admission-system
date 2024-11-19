65b7ca4b3d9b1329945135981963c5536a02e877
Mario <mariotek2002@gmail.com>
2024-11-19 19:23:30 +0800

    renamed dashboard home to dashboard applicants

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -5,7 +5,7 @@ import LoginView from '../views/LoginView.vue'
 import RegisterView from '../views/RegisterView.vue'
 import StudentPortalView from '../views/StudentPortalView.vue'
 import DashboardView from '../views/DashboardView.vue'
-import DashboardHome from '../views/DashboardHome.vue'
+import DashboardApplicants from '../views/DashboardApplicants.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -41,8 +41,8 @@ const router = createRouter({
       component: DashboardView
     },
     {
-      path: '/dhome',
-      name: 'Dhome',
+      path: '/dashboard_applicants',
+      name: 'Applicants',
       component: DashboardHome
     },
     {
