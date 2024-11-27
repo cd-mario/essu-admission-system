aa5f790a179e6fb67c63799518ccd0126ca1ea1f
Mario <mariotek2002@gmail.com>
2024-11-27 21:43:32 +0800

    added the navbar and footer component directly to the form view

--- app/src/views/FormView.vue
+++ app/src/views/FormView.vue
@@ -1,86 +1,92 @@
-<script>
-
+<script setup>
+import Navbar from '../components/Navbar.vue'
+import Footer from '../components/Footer.vue'
 </script>
+
 <template>
-    <div class="wrapper">
-        <h1 class="title">ESSU Admission Form</h1>
-        <p class="note"><strong>Note: </strong>You must <strong>login</strong> to access the admission form.</p>
-        <p class="desc">Enter your following information below</p>
-        <hr>
-            <div class="cont">
-                    <form>
-                        <h3 class="title-2 name">Name</h3>
-                        <div class="form-wrapper">
-                            <div class="form-group">
-                              <label for="f_name">First name</label>
-                              <input type="text" class="form-control" id="f_name" placeholder="Enter first name">
-                            </div>
-                            <div class="form-group">
-                              <label for="m_name">Midldle name</label>
-                              <input type="text" class="form-control" id="m_name" placeholder="Enter middle name">
-                            </div>
-                            <div class="form-group">
-                              <label for="l_name">Last name</label>
-                              <input type="text" class="form-control" id="l_name" placeholder="Enter last name">
-                            </div>
-                        </div>
-                        <hr>
-                        <h3 class="title-2">Personal</h3>
-                        <div class="form-wrapper personal">
-                            <div class="form-group">
-                              <label for="email">Email</label>
-                              <input type="email" class="form-control" id="email" placeholder="Enter email">
-                            </div>
-                            <div class="form-group">
-                              <label for="bday">Date of birth</label>
-                              <input type="date" class="form-control" id="bday" placeholder="Enter birth date">
-                            </div>
-                            <div class="form-group">
-                              <label for="num">Mobile number</label>
-                              <input type="number" class="form-control" id="num" placeholder="Enter mobile number">
-                            </div>
-                            <div class="form-group">
-                              <label for="address">Address</label>
-                              <input type="text" class="form-control" id="address" placeholder="Enter address">
-                            </div>
-                        </div>
-                        <hr>
-                        <h3 class="title-2">School</h3>
-                        <div class="form-wrapper school">
-                            <div class="form-group">
-                              <label for="primary">Primary school</label>
-                              <input type="text" class="form-control" id="primary" placeholder="Enter primary school">
-                            </div>
-                            <div class="form-group">
-                              <label for="secondary">Secondary school</label>
-                              <input type="text" class="form-control" id="secondart" placeholder="Enter secondary school">
-                            </div>
-                            <div class="form-group">
-                              <label for="senior">Enter senior high school</label>
-                              <input type="text" class="form-control" id="senior" placeholder="Enter senior high school">
-                            </div>
-                        </div>
-                        <hr>
-                        <h3 class="title-2">Admission</h3>
-                        <div class="form-wrapper school">
-                            <div class="form-group">
-                              <label for="f_choice">First choice</label>
-                              <input type="text" class="form-control" id="f_choice" placeholder="Enter first choice">
-                            </div>
-                            <div class="form-group">
-                              <label for="s_choice">Second choice</label>
-                              <input type="text" class="form-control" id="s_choice" placeholder="Enter second choice">
-                            </div>
-                            <div class="form-group">
-                              <label for="gpa">GPA</label>
-                              <input type="text" class="form-control" id="s_choice" placeholder="Enter grade point average">
-                            </div>
-                        </div>
-                        <div class="right">
-                            <button type="submit" class="btn btn-primary">Submit</button>
-                        </div>
-                      </form>
-            </div>
+    <div>
+      <Navbar />
+      <div class="wrapper">
+          <h1 class="title">ESSU Admission Form</h1>
+          <p class="note"><strong>Note: </strong>You must <strong>login</strong> to access the admission form.</p>
+          <p class="desc">Enter your following information below</p>
+          <hr>
+              <div class="cont">
+                      <form>
+                          <h3 class="title-2 name">Name</h3>
+                          <div class="form-wrapper">
+                              <div class="form-group">
+                                <label for="f_name">First name</label>
+                                <input type="text" class="form-control" id="f_name" placeholder="Enter first name">
+                              </div>
+                              <div class="form-group">
+                                <label for="m_name">Midldle name</label>
+                                <input type="text" class="form-control" id="m_name" placeholder="Enter middle name">
+                              </div>
+                              <div class="form-group">
+                                <label for="l_name">Last name</label>
+                                <input type="text" class="form-control" id="l_name" placeholder="Enter last name">
+                              </div>
+                          </div>
+                          <hr>
+                          <h3 class="title-2">Personal</h3>
+                          <div class="form-wrapper personal">
+                              <div class="form-group">
+                                <label for="email">Email</label>
+                                <input type="email" class="form-control" id="email" placeholder="Enter email">
+                              </div>
+                              <div class="form-group">
+                                <label for="bday">Date of birth</label>
+                                <input type="date" class="form-control" id="bday" placeholder="Enter birth date">
+                              </div>
+                              <div class="form-group">
+                                <label for="num">Mobile number</label>
+                                <input type="number" class="form-control" id="num" placeholder="Enter mobile number">
+                              </div>
+                              <div class="form-group">
+                                <label for="address">Address</label>
+                                <input type="text" class="form-control" id="address" placeholder="Enter address">
+                              </div>
+                          </div>
+                          <hr>
+                          <h3 class="title-2">School</h3>
+                          <div class="form-wrapper school">
+                              <div class="form-group">
+                                <label for="primary">Primary school</label>
+                                <input type="text" class="form-control" id="primary" placeholder="Enter primary school">
+                              </div>
+                              <div class="form-group">
+                                <label for="secondary">Secondary school</label>
+                                <input type="text" class="form-control" id="secondart" placeholder="Enter secondary school">
+                              </div>
+                              <div class="form-group">
+                                <label for="senior">Enter senior high school</label>
+                                <input type="text" class="form-control" id="senior" placeholder="Enter senior high school">
+                              </div>
+                          </div>
+                          <hr>
+                          <h3 class="title-2">Admission</h3>
+                          <div class="form-wrapper school">
+                              <div class="form-group">
+                                <label for="f_choice">First choice</label>
+                                <input type="text" class="form-control" id="f_choice" placeholder="Enter first choice">
+                              </div>
+                              <div class="form-group">
+                                <label for="s_choice">Second choice</label>
+                                <input type="text" class="form-control" id="s_choice" placeholder="Enter second choice">
+                              </div>
+                              <div class="form-group">
+                                <label for="gpa">GPA</label>
+                                <input type="text" class="form-control" id="s_choice" placeholder="Enter grade point average">
+                              </div>
+                          </div>
+                          <div class="right">
+                              <button type="submit" class="btn btn-primary">Submit</button>
+                          </div>
+                        </form>
+              </div>
+      </div>
+      <Footer />
     </div>
 </template>
 <style scoped>
