73a11de0744322ded4d9425bd2109ed259e2bcd4
Mario <mariotek2002@gmail.com>
2024-12-01 21:05:17 +0800

    added more inputs for registration

--- app/backend/app/Http/Controllers/RegisterController.php
+++ app/backend/app/Http/Controllers/RegisterController.php
@@ -3,8 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
-    //
+    
+    public function store(Request $request)
+    {
+        $input = $request->all();
+
+        User::create([
+            'first_name' => $input['first_name'],
+            'last_name' => $input['last_name'],
+            'email' => $input['email'],
+            'password' => Hash::make($input['password'])
+        ]);
+
+
+        return response()->json(['status' => true,
+                                    'message' => "Registration successful"
+                                    
+        ]);
+    }
+
 }
