500c44012fe0e53012105577c91e6e96c76202ef
Mario <mariotek2002@gmail.com>
2024-12-02 19:24:58 +0800

    added api route for the login

--- app/backend/routes/api.php
+++ app/backend/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,7 +22,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('/register', [RegisterController::class, 'store']);
 
-// Route::post('/login', [LoginController::class, 'check']);
+Route::post('/login', [LoginController::class, 'check']);
 
 
 
